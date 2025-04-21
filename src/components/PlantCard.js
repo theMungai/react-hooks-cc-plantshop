843a05ecddfc8397e43e6b8c387e322854b64a07
theMungai <mungaithesimon@gmail.com>
2025-04-21 23:47:45 +0300

    Render data

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ image, name, price, inStock }) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {inStock ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(function(){
+    fetch("http://localhost:5500/plants")
+    .then((response) => response.json())
+    .then((data) => setPlants(data))
+    .catch((error) => console.log("Unable to fetch data", error))
+  }, [])
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard
+          key={plant.id}
+          image={plant.image}
+          name={plant.name}
+          price={plant.price}
+          inStock={plant.inStock}
+        />
+      ))}
+    </ul>
   );
 }
 
