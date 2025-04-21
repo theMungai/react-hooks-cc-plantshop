1af12eaeae5190f2ddccf4ef3b6fb5b9611bb31a
theMungai <mungaithesimon@gmail.com>
2025-04-22 01:22:17 +0300

    React Fetch and Post

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -36,9 +36,9 @@ function NewPlantForm() {
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleChange} />
-        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} />
-        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleChange} />
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleChange} required/>
+        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} required/>
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleChange} required/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
